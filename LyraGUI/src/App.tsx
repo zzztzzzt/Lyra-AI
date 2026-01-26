ae2e5cb751945015acd9704d49897858585ed32a
zzztzzzt <180073683+zzztzzzt@users.noreply.github.com>
2026-01-26 22:17:18 +0800

    [update] update showcase area, to let GUI shows more color, like middle color of gradient, the main color, main color gradient etc

--- LyraGUI/src/App.tsx
+++ LyraGUI/src/App.tsx
@@ -12,6 +12,24 @@ export interface OklchState {
 
 const toStr = (col: OklchState) => `oklch(${(col.l * 100).toFixed(0)}% ${col.c.toFixed(3)} ${col.h})`;
 
+const getMidColor = (colorA: OklchState, colorB: OklchState): OklchState => {
+  let h1 = colorA.h;
+  let h2 = colorB.h;
+
+  let diff = h2 - h1;
+  if (diff > 180) {
+    h2 -= 360;
+  } else if (diff < -180) {
+    h2 += 360;
+  }
+
+  let hMid = (h1 + h2) / 2;
+  // Ensure the result returns to the positive range of 0-360
+  hMid = (hMid + 360) % 360;
+
+  return { l: (colorA.l + colorB.l) / 2, c: (colorA.c + colorB.c) / 2, h: hMid };
+};
+
 function App() {
   const [colorM, setColorM] = useState<OklchState>({ l: 0.7, c: 0.15, h: 250 });
   const [colorA, setColorA] = useState<OklchState>({ l: 0.7, c: 0.15, h: 250 });
@@ -20,11 +38,17 @@ function App() {
   const accentM = useMemo(() => toStr(colorM), [colorM]);
   const accentA = useMemo(() => toStr(colorA), [colorA]);
   const accentB = useMemo(() => toStr(colorB), [colorB]);
-  const mainGradient = `linear-gradient(90deg, ${accentA}, ${accentB})`;
+  const mainGradient = `linear-gradient(90deg in oklch, ${accentA}, ${accentB})`;
+  
+  const midColor = useMemo(() => getMidColor(colorA, colorB), [colorA, colorB]);
+  const accentMid = useMemo(() => toStr(midColor), [midColor]);
 
   return (
     <div className='lg:flex lg:flex-row-reverse'>
-      <div className='max-lg:max-w-100 lg:w-1/4 lg:min-w-100 lg:h-screen max-lg:mx-auto overflow-auto lg:border-l-solid border-gray-300 font-prosto-one'>
+      <div
+        className='max-lg:max-w-100 lg:w-1/4 lg:min-w-100 lg:h-screen max-lg:mx-auto overflow-auto lg:border-l-solid font-prosto-one'
+        style={{ borderColor: accentM }}
+      >
         <div className='mt-6 text-center text-3xl md:text-2.5xl'>
           <span className='text-transparent bg-clip-text' style={{ backgroundColor: accentM }}>Lyra </span>
           <span className='text-transparent bg-clip-text' style={{ backgroundImage: mainGradient }}>training system</span>
@@ -45,7 +69,11 @@ function App() {
         />
       </div>
       <div className='relative w-full lg:h-screen'>
-        <ShowcaseArea gradient={mainGradient} />
+        <ShowcaseArea
+          gradient={mainGradient}
+          midColor={accentMid}
+          accentM={accentM}
+        />
       </div>
     </div>
   )
--- LyraGUI/src/ShowcaseArea.tsx
+++ LyraGUI/src/ShowcaseArea.tsx
@@ -2,12 +2,20 @@ import React from 'react';
 
 interface Props {
   gradient: string;
+  midColor: string;
+  accentM: string;
 }
 
-const ShowcaseArea: React.FC<Props> = (({ gradient }) => {
+const ShowcaseArea: React.FC<Props> = (({ gradient, midColor, accentM }) => {
   return(
-    <div className='relative flex justify-center items-center w-full h-200 lg:h-full'>
-      <div className='w-1/2 h-1/2 rounded-xl' style={{ backgroundImage: gradient }}></div>
+    <div className='relative flex flex-col-reverse lg:flex-row justify-center items-center w-full h-200 lg:h-full'>
+      <div className='w-9/10 max-lg:max-w-140 lg:w-1/2 h-1/2 flex justify-center items-center rounded-xl' style={{ backgroundImage: gradient }}>
+        <div className='w-2/3 h-2/3 flex justify-center items-center bg-white rounded-lg'>
+          <div className='w-5/6 h-5/6 rounded-lg' style={{ backgroundImage: `linear-gradient(90deg in oklch, ${midColor}, white)` }}></div>
+        </div>
+      </div>
+
+      <div className='max-lg:mb-6 lg:ml-6 h-10 lg:h-30 w-9/10 max-lg:max-w-140 lg:w-15 rounded-md' style={{ backgroundColor: accentM }}></div>
     </div>
   );
 });
