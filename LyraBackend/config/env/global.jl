865872c3fd736cd323a046e8a536ba1a3ea0adcd
zzztzzzt <180073683+zzztzzzt@users.noreply.github.com>
2026-02-03 15:40:46 +0800

    [update] update README & Lyra Backend ENV for Lyra 1.5

--- LyraBackend/config/env/global.jl
+++ LyraBackend/config/env/global.jl
@@ -8,4 +8,4 @@ ENV["ROOT_PARENT"] = dirname(APP_ROOT)
 ENV["EXTERNAL_TRAINING_DATA_PATH"] = "training_data/color_data.jld2"
 
 ENV["EXTERNAL_CUSTOM_AI_MODEL_PATH"] = "models/trained_color_model.jld2"
-ENV["EXTERNAL_NEWEST_LYRA_MODEL_PATH"] = "models/Lyra1.0.jld2"
+ENV["EXTERNAL_NEWEST_LYRA_MODEL_PATH"] = "models/Lyra1.5.jld2"
